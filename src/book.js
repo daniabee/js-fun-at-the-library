9fc553fb474c0927e37ee050f5335dd271064b2e
Dani Bagley <bagley.a.danielle@gmail.com>
2022-08-24 22:15:05 -0600

    Add tested books.js, 11 passing, 7 failing

--- src/book.js
+++ src/book.js
@@ -1,20 +1,48 @@
-function createTitle(title) {
+function createTitle(title, addidions = null) {
+  if (addidions === null) {
   return title;
-}
+  } else {
+  return additions;
+  }
+};
 
 module.exports = {
-  createTitle : function (title) {
+
+  /* createTitle : function (title, additions = null) {
+    if (additions === null) {
     return title;
+    } else {
+    return additions;
+    }
+  },*/
+
+  createTitle : function (title) {
+    return `The ${title}`;
   },
-  buildMainCharacter : function
-  (newCharacterBuilt, newCharProperty, propertyValue) {
+
+  buildMainCharacter : function (name, age, pronouns) {
     return module.exports.newCharcaterBuilt = {
-      newCharProperty : propertyValue ,
+      name : name ,
+      age : age,
+      pronouns : pronouns,
     };
   },
-  saveReview : function (array,review) {
-    return array.push(review);
+
+/*  saveReview : function (array, review) {
+    if (array.includes(review) === false) {
+      return array.splice(review.length, 0 , review)
+    }
+  },*/
+
+  saveReview : function (review, array) {
+      for (var i  = 0; i < array.length; i++) {
+        if (array[i] != review) {
+          array2 = [review];
+          return array2.concat(array)
+        }
+      }
   },
+
   calculatePageCount : function (title) {
     var count = 0;
     words = title.split("");
@@ -22,11 +50,23 @@ module.exports = {
       count++;}
       return count * 20;
   },
-  writeBook : function(bookProp){
-    return bookProp
-  },
-  editBook : functgit ion(bookPageCount) {
-    var count = booksPageCount/3;
+
+/*  writeBook : function(bookTitle, bookCharacter, genre){
+    return book = {
+      bookTitle : bookTitle,
+      bookCharacter : bookCharacter,
+      bookGenre : bookGenre,
+    };
+  },*/
+
+  writeBook : function (property,value) {
+    var book = module.exports.book[property]
+    return book = value;
+},
+
+  editBook : function(pageCount) {
+    var quarter = pageCount/4;
+    var count = pageCount - quarter;
     return count
   }
 }
