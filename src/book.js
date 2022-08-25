51299e0d5584124803c78be6241d20445f64e6f2
Dani Bagley <bagley.a.danielle@gmail.com>
2022-08-25 15:04:18 -0600

    Add passing fucntions to iteration 1, books.js

--- src/book.js
+++ src/book.js
@@ -1,69 +1,47 @@
-function createTitle(title, addidions = null) {
-  if (addidions === null) {
-  return title;
-  } else {
-  return additions;
+function createTitle(title, additions = null) {
+  return `The ${title}`
+};
+
+function buildMainCharacter (name, age, pronouns) {
+  return module.exports.newCharcaterBuilt = {
+    name : name ,
+    age : age,
+    pronouns : pronouns,
   }
 };
 
-module.exports = {
-
-  /* createTitle : function (title, additions = null) {
-    if (additions === null) {
-    return title;
-    } else {
-    return additions;
-    }
-  },*/
-
-  createTitle : function (title) {
-    return `The ${title}`;
-  },
-
-  buildMainCharacter : function (name, age, pronouns) {
-    return module.exports.newCharcaterBuilt = {
-      name : name ,
-      age : age,
-      pronouns : pronouns,
-    };
-  },
-  
-  saveReview : function (newReview , array) {
-    if (array.includes(newReview) === false) {
-      return array.push(newReview);
-    }
-},
-
-  calculatePageCount : function (title) {
-    var count = 0;
-    words = title.split("");
-    for (var i = 0; i < words.length; i++) {
-      count++;}
-      return count * 20;
-  },
-
-/*  writeBook : function(bookTitle, bookCharacter, genre){
-    return book = {
-      bookTitle : bookTitle,
-      bookCharacter : bookCharacter,
-      bookGenre : bookGenre,
-    };
-  },*/
-
-/*  writeBook : function (property,value) {
-    var book = module.exports.book
-    return book.property = value;
-}, */
-
-  writeBook : function (property,value) {
-    return module.exports.newBook = {
-      property : value ,
-    }
-  },
-
-  editBook : function(pageCount) {
-    var quarter = pageCount/4;
-    var count = pageCount - quarter;
-    return count
+function saveReview (newReview, array) {
+  if (array.includes(newReview) === false) {
+    return array.push(newReview);
   }
+};
+
+function calculatePageCount (title) {
+  var count = 0;
+  words = title.split("");
+  for (var i = 0; i < words.length; i++) {
+    count++;}
+    return count * 20;
+};
+
+function writeBook (bookTitle, bookCharacter, genre) {
+    return newBook = {
+      title : bookTitle,
+      mainCharacter : bookCharacter,
+      genre : genre,
+      pageCount : calculatePageCount(bookTitle)
+    }
+};
+
+function editBook (book) {
+  book.pageCount = (book.pageCount/4) * 3
+}
+
+module.exports = {
+  createTitle,
+  buildMainCharacter,
+  saveReview,
+  calculatePageCount,
+  writeBook,
+  editBook
 }
