cf79a6800418c6c4f80ecb10b3fbace0eb7468d8
Dani Bagley <bagley.a.danielle@gmail.com>
2022-08-24 18:16:47 -0600

    Add first edits to book.js, 0/18 passed

--- src/book.js
+++ src/book.js
@@ -1,12 +1,32 @@
-function createTitle() {
-  
+function createTitle(title) {
+  return title;
 }
 
 module.exports = {
-  createTitle,
-  // buildMainCharacter,
-  // saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  createTitle : function (title) {
+    return title;
+  },
+  buildMainCharacter : function
+  (newCharacterBuilt, newCharProperty, propertyValue) {
+    return module.exports.newCharcaterBuilt = {
+      newCharProperty : propertyValue ,
+    };
+  },
+  saveReview : function (array,review) {
+    return array.push(review);
+  },
+  calculatePageCount : function (title) {
+    var count = 0;
+    words = title.split("");
+    for (var i = 0; i < words.length; i++) {
+      count++;}
+      return count * 20;
+  },
+  writeBook : function(bookProp){
+    return bookProp
+  },
+  editBook : functgit ion(bookPageCount) {
+    var count = booksPageCount/3;
+    return count
+  }
 }
