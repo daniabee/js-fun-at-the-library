5ed6d053193d3025265801be6a3f127e135289bc
Dani Bagley <bagley.a.danielle@gmail.com>
2022-08-25 16:52:30 -0600

    Add responses to shelf test iteration, still unfinished.

--- src/librarian.js
+++ src/librarian.js
@@ -1,7 +1,16 @@
 class Librarian {
   constructor() {
-    
+
+    function shelfBook () {
+
+    }
+
+
+
   }
 }
 
 module.exports = Librarian;
+
+
+//npm test test/shelf-test.js
--- src/shelf.js
+++ src/shelf.js
@@ -1,7 +1,50 @@
 
+function shelfBook (book, shelf) {
+  if (shelf.length < 3) {
+    return shelf.unshift(book);
+  }
+}
+
+function unshelfBook (name, shelf) {
+  var index = shelf.indexOf(name);
+  shelf = shelf.splice(index, 1);
+    return shelf
+}
+
+function listTitles (shelf) {
+  var list = []
+  for (var i = 0; i < shelf.length; i++) {
+    list.push((shelf[i].title))
+  }
+  return list
+}
+
+/* function searchShelf (shelf, name) {
+    if (shelf.includes(name) === true) {
+      return true;
+    } else {
+      return false;
+    }
+} */
+
+function searchShelf (shelf, name) {
+  for (var i = 0; i < shelf.length ; i++) {
+    if(shelf[i].title == name) {
+      return true
+    } else {
+      return false
+    }
+  }
+}
+
+
 module.exports = {
-  // shelfBook,
-  // unshelfBook,
-  // listTitles,
-  // searchShelf
+  shelfBook,
+  unshelfBook,
+  listTitles,
+  searchShelf
 };
+
+
+
+// npm test test/shelf-test.js
