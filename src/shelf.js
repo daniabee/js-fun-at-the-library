88258c31d9d193d27b9ea193e71c1cb3071e3eca
Dani Bagley <bagley.a.danielle@gmail.com>
2022-08-25 19:20:06 -0600

    Add completed test of shelf.js, iteration 2

--- src/shelf.js
+++ src/shelf.js
@@ -5,36 +5,38 @@ function shelfBook (book, shelf) {
   }
 }
 
-function unshelfBook (name, shelf) {
+/*function unshelfBook (name, shelf) {
   var index = shelf.indexOf(name);
   shelf = shelf.splice(index, 1);
     return shelf
-}
+}*/
+
+function unshelfBook (book, shelf) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title == book){
+      shelf.splice(i, 1);
+      }
+    }
+    return shelf
+  }
 
 function listTitles (shelf) {
   var list = []
   for (var i = 0; i < shelf.length; i++) {
-    list.push((shelf[i].title))
+    list.push(shelf[i].title)
   }
-  return list
+  return list.join(', ')
 }
 
-/* function searchShelf (shelf, name) {
-    if (shelf.includes(name) === true) {
-      return true;
+function searchShelf (shelf, book) {
+  for (var i = 0; i < shelf.length; i++) {
+    if (shelf[i].title.toLowerCase() == book.toLowerCase()) {
+      var final = true
     } else {
-      return false;
-    }
-} */
-
-function searchShelf (shelf, name) {
-  for (var i = 0; i < shelf.length ; i++) {
-    if(shelf[i].title == name) {
-      return true
-    } else {
-      return false
+      var final = false
     }
   }
+  return final
 }
 
 
